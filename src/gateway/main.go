f90ea6f3d1f0d8b0b7faa8eaec738df7a284bac8
piotr.laczykowski <piotr.laczykowski@example.com>
2026-03-28 08:58:30 +0100

    refactor(gateway): extract metrics vars to metrics.go

--- src/gateway/main.go
+++ src/gateway/main.go
@@ -32,28 +32,11 @@ import (
 	"github.com/piotrlaczykowski/emdexer/version"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/propagation"
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promauto"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/qdrant/go-client/qdrant"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health/grpc_health_v1"
 )
-
-var searchEmptyResults = promauto.NewCounterVec(prometheus.CounterOpts{
-	Name: "emdexer_gateway_search_empty_results_total",
-	Help: "Number of search requests that returned zero results",
-}, []string{"namespace", "mode"})
-
-var topologyNamespacesKnown = promauto.NewGauge(prometheus.GaugeOpts{
-	Name: "emdexer_gateway_topology_namespaces_known",
-	Help: "Number of namespaces currently known from the node registry",
-})
-
-var topologyNodesKnown = promauto.NewGauge(prometheus.GaugeOpts{
-	Name: "emdexer_gateway_topology_nodes_known",
-	Help: "Number of nodes currently known from the node registry",
-})
 
 type Server struct {
 	reg          registry.NodeRegistry
---
+++ src/gateway/metrics.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+)
+
+var searchEmptyResults = promauto.NewCounterVec(prometheus.CounterOpts{
+	Name: "emdexer_gateway_search_empty_results_total",
+	Help: "Number of search requests that returned zero results",
+}, []string{"namespace", "mode"})
+
+var topologyNamespacesKnown = promauto.NewGauge(prometheus.GaugeOpts{
+	Name: "emdexer_gateway_topology_namespaces_known",
+	Help: "Number of namespaces currently known from the node registry",
+})
+
+var topologyNodesKnown = promauto.NewGauge(prometheus.GaugeOpts{
+	Name: "emdexer_gateway_topology_nodes_known",
+	Help: "Number of nodes currently known from the node registry",
+})
